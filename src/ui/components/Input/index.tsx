725ba770484e3184d18a1bcdd0c93aad435ab9ae
jacky <softwarecheng@126.com>
2024-07-04 21:02:40 +0800

    1.3.3 -> 1.3.4

--- src/ui/components/Input/index.tsx
+++ src/ui/components/Input/index.tsx
@@ -316,7 +316,7 @@ export const AddressInput = (props: InputProps) => {
             color="yellow"
             text={'More details'}
             onClick={() => {
-              window.open(`https://docs.unisat.io/unisat-wallet/name-recognized-and-resolved`);
+              window.open('https://ordx.space/sat20-wallet/name-recognized-and-resolved');
             }}
           />
           <Text preset="sub" size="sm" text={')'} />

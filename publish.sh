40f2ece9727e477019c493feb25fc33ca5824c3f
softwarecheng <softwarecheng@126.com>
2024-08-29 15:09:47 +0800

    fix: publish.sh

--- publish.sh
+++ publish.sh
@@ -9,9 +9,28 @@ if [ -z "$1" ]; then
 fi
 
 VERSION=$1
+REPO="sat20-labs/extension"
+ZIP_FILE="dist/sat20-chrome-mv3-$VERSION.zip"
+
+REQUIRED_NODE_VERSION="v16.10."
+NODE_VERSION=$(node -v)
+if [[ $NODE_VERSION != $REQUIRED_NODE_VERSION* ]]; then
+    echo "Error: Node.js version must be $REQUIRED_NODE_VERSION.x"
+    exit 1
+fi
 
 git checkout master
 git pull origin master
+yarn build:chrome:mv3
+
+if [ ! -f "$ZIP_FILE" ]; then
+    echo "Error: File $ZIP_FILE does not exist."
+    exit 1
+fi
+
+git add .
+git commit -m "feature: publish.sh for create tag"
+git push
 
 if git rev-parse "$VERSION" >/dev/null 2>&1; then
     echo "Local tag $VERSION already exists. Deleting it..."
@@ -27,3 +46,8 @@ git tag "$VERSION"
 git push origin "$VERSION"
 
 echo "Tag $VERSION created and pushed to origin."
+
+gh release create "$VERSION" --repo "$REPO" --title "Release $VERSION" --notes "Release notes for $VERSION"
+
+gh release upload "$VERSION" "$ZIP_FILE" --repo "$REPO"
+echo "File $ZIP_FILE uploaded to release $VERSION."
